62200f111385391d1fd2729688d2ca16fb043774
mpark123 <matthew_h_park@brown.edu>
2021-11-28 23:01:12 -0500

    adding changes

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -12,12 +12,9 @@ def csv_data(filepath):
     csv_data = pd.read_csv(filepath, parse_dates=["Date"])
     return csv_data
 
-def join_commodities(commodities_dict):
-    pass
-
-def get_data(stock_filepath, commodities_filepath):
+def sort_data(stock_filepath, commodities_filepath):
     """
-    Creates dataframes and turns them into usable matrices for the model
+    Creates dataframes and turns them into sorted dataframes of equal lengths
     :param filepaths: paths to csv files
     :returns: stock data and commodities data to use for the model
     """
@@ -31,7 +28,17 @@ def get_data(stock_filepath, commodities_filepath):
     stock_data = stock_data.sort_values(by=['Date'])
     commodities_data = commodities_data.sort_values(by=['Date'])
 
-    return stock_data, commodities_data
+    stock_dates = stock_data['Date'].tolist()
+    commodities_dates = commodities_data['Date'].tolist()
+    stock_dates_array = np.array(stock_dates)
+    commodities_dates_array = np.array(commodities_dates)
+
+    shared_dates = np.intersect1d(stock_dates_array, commodities_dates_array)
+
+    stonk = stock_data[stock_data['Date'].isin(shared_dates)]
+    commie = commodities_data[commodities_data['Date'].isin(shared_dates)]
+
+    return stonk, commie
 
 def drop_column(stock_data, commodities_data, column_name):
     """
@@ -46,7 +53,7 @@ def drop_column(stock_data, commodities_data, column_name):
 
     return updated_stocks, updated_commodities
 
-def modify_historical(stock_data, commodities_data, start_date, end_date)
+def modify_historical(stock_data, commodities_data, start_date, end_date):
     """
     Updates both stock and commodities dataframe to include only desired historical range
     :param stock_data: dataframe containing historical SPY data
@@ -61,5 +68,38 @@ def modify_historical(stock_data, commodities_data, start_date, end_date)
     return filtered_stock, filtered_commodities
 
 
+def get_data(stock_filepath, commodities_filepath, train_start_date, train_end_date, test_start_date, test_end_date):
+    """
+    Updates both stock and commodities dataframe to include only desired historical range
+    :param stock_filepath: filepath to SPY .CSV file
+    :param commodities_filepath: filepath to Invesco DBC .CSV file
+    :param train_start_date: string that represents specific date to have as first data point (inclusive) for training
+    :param train_end_date: string that represents specific date to have as last data point (inclusive) for training
+    :param test_start_date: string that represents specific date to have as first data point (inclusive) for testing
+    :param test_end_date: string that represents specific date to have as last data point (inclusive) for testing
+    :returns: 
+        train data which corresponds to a 2-D numpy array where each array corresponds to a daily measurement of Invesco DBC (Open, High, Low, Close, Volume)
+        train labels which corresponds to a 2-D numpy array where each array corresponds to daily measurement of SPY (Close)
+        test data which corresponds to a 2-D numpy array where each array corresponds to a daily measurement of Invesco DBC (Open, High, Low, Close, Volume)
+        test labels which corresponds to a 2-D numpy array where each array corresponds to daily measurement of SPY (Close)
+    """
 
-def convert
+    stock_data, commodities_data = sort_data(stock_filepath, commodities_filepath)
+
+    updated_stock_data, updated_commodities_data = drop_column(stock_data, commodities_data, 'Adj Close')
+
+    train_stocks, train_commodities = modify_historical(updated_stock_data, updated_commodities_data, train_start_date, train_end_date)
+    test_stocks, test_commodities = modify_historical(updated_stock_data, updated_commodities_data, test_start_date, test_end_date)
+
+    np_train_stocks = train_stocks.to_numpy()
+    np_train_commodities = train_commodities.to_numpy()
+    np_test_stocks = test_stocks.to_numpy()
+    np_test_commodities = test_commodities.to_numpy()
+
+    train_data = np_train_commodities[:, 1:]
+    train_labels = np_train_stocks[:, -2:-1]
+    test_data = np_test_commodities[:, 1:]
+    test_labels = np_test_stocks[:, -2:-1]
+
+    return train_data, train_labels, test_data, test_labels
+
